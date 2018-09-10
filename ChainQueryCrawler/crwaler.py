d9c50f99534661ea640c0a38a7c24af8e2600507
Meric <doa_s@live.de>
2018-09-10 19:12:52 +0200

    crwaler is a function and main.py have the right order

--- ChainQueryCrawler/crwaler.py
+++ ChainQueryCrawler/crwaler.py
@@ -9,28 +9,30 @@ from bs4 import BeautifulSoup
 import time 
 import json
 
+def start_crwaler(trans_hash):
+    #important not to wait until the page is complete
+    caps = DesiredCapabilities().FIREFOX
+    caps["pageLoadStrategy"] = "eager"  #  interactive
 
-#important not to wait until the page is complete
-caps = DesiredCapabilities().FIREFOX
-caps["pageLoadStrategy"] = "eager"  #  interactive
+    driver = webdriver.Firefox()
+    driver.get("http://chainquery.com/bitcoin-api/getrawtransaction")
 
-driver = webdriver.Firefox()
-driver.get("http://chainquery.com/bitcoin-api/getrawtransaction")
+    driver.find_element_by_css_selector("input[type='radio'][id='verbose'][value='1']").click()
 
-driver.find_element_by_css_selector("input[type='radio'][id='verbose'][value='1']").click()
+    input_element = driver.find_element_by_id("txid")
+    input_element.send_keys(trans_hash)
+    input_element.send_keys(Keys.ENTER)
+    input_element.submit()
+    time.sleep(0.5)
 
-input_element = driver.find_element_by_id("txid")
-input_element.send_keys("492fcdecf0803a458c0240b4947063d5496df6f9b95056f33de73e391110f805")
-input_element.send_keys(Keys.ENTER)
-input_element.submit()
-time.sleep(0.5)
+    soup = BeautifulSoup(driver.page_source,"html.parser")
 
-soup = BeautifulSoup(driver.page_source,"html.parser")
+    elements = soup.find_all("pre")
+    elements = str(elements[0]).strip()
+    elements = elements[5:-6]
+    json_element = json.loads(elements)
+    print('elment:' + str(json_element))
 
-elements = soup.find_all("pre")
-elements = str(elements[0]).strip()
-elements = elements[5:-6]
-json_element = json.loads(elements)
-print('elment:' + str(json_element))
+    driver.quit()
 
-driver.quit()
+    return json_element
--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from SQL import sqlite as sql
 from BlockParser import parser
+from ChainQueryCrawler import crwaler
 
 print("The block analysis begins.")
-
-sql.initTabel()
-sql.addBlock(2,"1961-10-25")
-sql.selectTable()
 
 #TODO starts the function for every .dat file in ./Blocks. also wie compare from the start number and finale number
 block_trans = parser.read_hash('Blocks/blk00003.dat')
 print("Hash Values:" + str(block_trans))
+
+json_file = crwaler.start_crwaler("492fcdecf0803a458c0240b4947063d5496df6f9b95056f33de73e391110f805")
+
+#TODO find OP_RESULT
+sql.initTabel()
+sql.addBlock(2,"1961-10-25")
+sql.selectTable()
