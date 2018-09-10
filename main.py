42bb5bf5e6a2f256f657a371a222286699731f10
Meric <doa_s@live.de>
2018-09-10 14:41:43 +0200

    add todo in main.py

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ from BlockParser import parser
 print("The block analysis begins.")
 
 sql.initTabel()
-
 sql.addBlock(2,"1961-10-25")
-
 sql.selectTable()
 
+#TODO starts the function for every .dat file in ./Blocks. also wie compare from the start number and finale number
 block_trans = parser.read_hash('Blocks/blk00003.dat')
 print("Hash Values:" + str(block_trans))
