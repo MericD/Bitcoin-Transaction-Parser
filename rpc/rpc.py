d6adda38c12912d91b55b67d9fa44872161d8049
Meric <doa_s@live.de>
2018-09-22 23:48:17 +0200

    no message

--- rpc/rpc.py
+++ rpc/rpc.py
@@ -8,32 +8,36 @@ rpc_password=config.CONFIG['rpc_password']
 rpc_ip=config.CONFIG['rpc_ip']
 rpc_port=config.CONFIG['rpc_port']
 
+
+
 def get_transactions(block_number):
     block_trans_dec = {}
 
     rpc_connection= start_connection_to_rpc()
-    
     block_hash = get_the_block_hash(rpc_connection, block_number)
-
     block_trans = get_all_transactions(rpc_connection, block_hash)
-
     trans_decoded = decoded_transactions(rpc_connection, block_trans)
-
     block_trans_dec.update({block_number : trans_decoded})
 
     return block_trans_dec
 
+
+
 def start_connection_to_rpc():
     logging.basicConfig()
     logging.getLogger("BitcoinRPC").setLevel(logging.DEBUG)
-
     rpc_connection = AuthServiceProxy("http://%s:%s@%s:%s"%(rpc_user, rpc_password, rpc_ip, rpc_port))
     return rpc_connection
+
+
+
 
 def get_the_block_hash(rpc_connection, block_number):
     #Step 1 Get the block hash
     block_hash = rpc_connection.getblockhash(block_number)
     return block_hash
+
+
 
 def get_all_transactions(rpc_connection,block_hash):
     #Step 2 get transactions of the block
@@ -51,7 +55,10 @@ def get_all_transactions(rpc_connection,block_hash):
     block_json = rpc_connection.getblock(block_hash)
     print("\n" + "-----------------------------------------------------" + str(block_json['time']) + "\n")
     block_trans = block_json['tx']
-    return block_trans, str(block_json['time'])
+    block_time = int(str(block_json['time']))
+    return block_trans, block_time
+
+
 
 def decoded_transactions(rpc_connection,block_trans):
     trans_decoded = {}
