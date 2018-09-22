e9d196559b3dae29ec39f0a86cb15398e5304ff1
Meric <doa_s@live.de>
2018-09-22 22:54:12 +0200

    documentation

--- Core/core.py
+++ Core/core.py
@@ -3,17 +3,34 @@ from SQL import sqlite as sql
 from rpc import rpc
 import time
 
+
+
+# find all transaction that contain an OP_RETURN field
+# use dictionary (key) that contains all blocks and their transactions
 def find_op_return(block_trans):
+    #initialize a dictionary 
     block_trans_result = {}
+    # find in dictionary-key ({block_number : trans_decoded}) all transactions with OP_RETURN
     for key, trans in block_trans.items():
-        trans_result = transaction_contens_op_return(trans)
+        trans_result = transaction_contains_op_return(trans)
         if len(trans_result) > 0:
+           # save found OP_RETURN in block_trans_result ({transactionID : content of OP_RETURN})
             block_trans_result.update({key:trans_result})
+            #return new dictionary ({transactionID : content of OP_RETURN})
     return block_trans_result
 
+
+
+# save all searched information in database file _databaseFIle
+# find_block_trans contains ({transactionID : content of OP_RETURN})
 def save_result_in_databse(__databaseFile, find_block_trans):
+    # opens a connection to the SQLite database file __databaseFile
     connection = sqlite3.connect(__databaseFile)
+    # initialized SQL tables store in file __databaseFile
     sql.initTabel(connection)
+
+print("WHAT :" + find_block_trans)
+    # search in dictionary ({{block-number : content of OP_RETURN}}) 
     for key_b, trans in find_block_trans.items():
         block_number=key_b
         create_date=time.ctime(1410356926)
@@ -30,25 +47,43 @@ def save_result_in_databse(__databaseFile, find_block_trans):
 
     connection.close()
 
-def transaction_contens_op_return(trans):
+
+
+# filter all transactions that contains an OP_RETURN
+def transaction_contains_op_return(trans):
+    #initialize a dictionary 
     trans_result = {}
+    # find in dictionary-key ({transactionID : decoded raw transaction information}) 
     for key, value in trans.items():
+        # rearch in decoded raw transaction information in "vout" for OP_RETURN field
         for i in range(len(value["vout"])):
+            #save found OP_RETURN in dictionary ({transactionID: OP_RETURN content})
             op_return = get_op_return(value)
             if "" != op_return:
+                # update dictionary ({transactionID: OP_RETURN content}) 
+                # if more than one OP_RETURN field is found in a transaction
                 trans_result.update({key:value})
+    # return dictionary ({transactionID: OP_RETURN content})
     return trans_result
 
+
+
+# get OP_RETURN fields and content of them in a transaction
+# value contains decoded raw transaction information 
 def get_op_return(value):
     op_return = ""
+    # search in "vout" (decoded raw transaction information) for OP_RETURN 
     for i in range(len(value["vout"])):
         potential_op_return = value["vout"][i]["scriptPubKey"]["asm"]
         #if potential_op_return.startswith('0496b53'): #For Block 1 to find a transcation
+        # if you find a 'OP_RETURN' string in "vout"
         if potential_op_return.startswith('OP_RETURN'):
-            #add every op_return to the object
+            # if no more OP_RETURN is found return the object
             if "" == op_return:
                 op_return = str(potential_op_return)
+
+             # else add every OP_RETURN to the object if one more is found 
             else:
                 op_return = op_return + ", " + str(potential_op_return)
-
+    # return string of found OP_RETURN fields
     return op_return 
--- SQL/sqlite.py
+++ SQL/sqlite.py
@@ -1,35 +1,35 @@
 from SQL import querys as qy
 import sqlite3
 
+# initialise the two tables for database 
 def initTabel(connection):
     cursor = connection.cursor()
-
     __execute_command(cursor,qy.get_create_table_query())
     __execute_command(cursor,qy.get_create_tx_table_query())
-    
-    # never forget this, if you want the changes to be saved:
+    # never forget this, if you want that the changes are saved
     connection.commit()
 
+
+# adding a block to table-block with corresponding information
 def addBlock(connection, block_number, create_date):
     cursor = connection.cursor()
-
     print("Add block to table")
-    
     __execute_command(cursor,qy.get_add_block_query(block_number,create_date))
-    
-
-    # never forget this, if you want the changes to be saved:
+    # never forget this, if you want that the changes are saved
     connection.commit() 
 
+
+# adding a transaction to table-tx with corresponding information
 def addTrans(connection,  block_number, transaction_id, version, tx_size ,vin_size, vout_size,op_return):
     cursor = connection.cursor()
-
     print("Add trans to table")
     __execute_command(cursor,qy.get_add_tx_query(transaction_id, version, tx_size ,vin_size, vout_size, op_return, block_number))
-    
-    # never forget this, if you want the changes to be saved:
+    # never forget this, if you want that the changes are saved
     connection.commit()
 
+
+# catch error if a block was added in table-block before
+# catch error if a transaction was added in table-tx with corresponding information before
 def __execute_command(cursor, sql_command):
     try:
         sql_command = str(sql_command)
