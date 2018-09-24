113ca046d0b7a06def468207ae97098febf8a093
Meric <doa_s@live.de>
2018-09-24 12:27:40 +0200

    no message

--- SQL/querys.py
+++ SQL/querys.py
@@ -27,7 +27,6 @@ __create_tx_table_qy = """CREATE TABLE IF NOT EXISTS tx (
 	tx_size integer,
 	vin_size integer,
 	vout_size integer,
-    tx_time integer,
     tx_value text,
 	op_return text,
 	block_number integer,
@@ -44,9 +43,9 @@ __add_block_qy = """INSERT INTO block
 
 # SQL statment to add corresponding transaction information in table-tx 
 __add_tx_qy = """INSERT INTO tx 
-    (transaction_id, version, tx_size ,vin_size, vout_size, tx_time,  tx_value, op_return, block_number) 
+    (transaction_id, version, tx_size ,vin_size, vout_size,  tx_value, op_return, block_number) 
     VALUES 
-    ("{transaction_id}", {version}, {tx_size}, {vin_size}, {vout_size}, {tx_time}, "{tx_value}", "{op_return}", {block_number})
+    ("{transaction_id}", {version}, {tx_size}, {vin_size}, {vout_size}, "{tx_value}", "{op_return}", {block_number})
 ;"""
 
 
@@ -63,5 +62,5 @@ def get_create_tx_table_query():
     return __create_tx_table_qy
 
 # Returns the query to add a transaction and the information of it to the table-tx
-def get_add_tx_query(tx_id, tx_v, tx_s ,tx_vin_size, tx_vout_size, tx_t, tx_val, tx_op_return, tx_block_id):
-    return __add_tx_qy.format(transaction_id=tx_id, version=tx_v, tx_size=tx_s, vin_size=tx_vin_size, vout_size=tx_vout_size, tx_time=tx_t, tx_value=tx_val, op_return=tx_op_return, block_number=tx_block_id) 
+def get_add_tx_query(tx_id, tx_v, tx_s ,tx_vin_size, tx_vout_size, tx_val, tx_op_return, tx_block_id):
+    return __add_tx_qy.format(transaction_id=tx_id, version=tx_v, tx_size=tx_s, vin_size=tx_vin_size, vout_size=tx_vout_size, tx_value=tx_val, op_return=tx_op_return, block_number=tx_block_id) 
