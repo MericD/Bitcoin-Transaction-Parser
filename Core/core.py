c858fbbc11abb271aef37553de391e8c83600188
Meric <doa_s@live.de>
2018-09-24 22:24:38 +0200

    no message

--- Core/core.py
+++ Core/core.py
@@ -48,7 +48,7 @@ def save_result_in_database(__databaseFile, find_block_trans):
             vin_size = len(value['vin'])
             vout_size = len(value['vout'])
             tx_time = value['time']
-            tx_value = "0" #TODO
+            tx_value = str(value['value'])
             op_return = get_op_return(value)
             # add information for transaction from dictionary value 
             # ({key->block-number : value -> decoded raw transaction information}) to the created SQL table
