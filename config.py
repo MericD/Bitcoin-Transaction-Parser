f4101898231a5b396dc901a34cb7e754f1d6a7a6
Meric <doa_s@live.de>
2018-09-25 15:35:19 +0200

    no message

--- config.py
+++ config.py
@@ -7,7 +7,7 @@
 CONFIG = {
     'database_file_name': 'blockchain.db',
     'start_block': 1,
-    'end_block':542998,
+    'end_block':319998,
     'rpc_user':'rpc',
     'rpc_password':'bitmaster',
     'rpc_ip':'127.0.0.1',
