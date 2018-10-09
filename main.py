db0873a5798eefe98366e7d60c47f666c0b6aa02
Meric <doa_s@live.de>
2018-10-09 13:43:41 +0200

    bugfix - write not same time in sqlite database otherwise some data are lost before storing in DB

--- main.py
+++ main.py
@@ -7,28 +7,46 @@ import config
 def thread_fun(start,end):
   ###Parameter
   # name of the database 
-  __databaseFile = config.CONFIG['database_file_name']
+  #__databaseFile = config.CONFIG['database_file_name']
 
 
   # the first block to analyze
-  __start_block = start
+  __start_block = 1
   #config.CONFIG['start_block']
 
   # the last block to analyze
-  __end_block = end
+  __end_block = 2
   #config.CONFIG['end_block']
 
   # get all Blocks and transactions in range of __start_block to __end_block
   block_trans = {}
 
-  while __start_block < __end_block + 1:
-    block_trans.update(rpc.get_transactions(__start_block)[0])
-    __start_block += 1
+  #for i in range(5):
+    #print(str(start) + "  -  " + str(end))
+
+  #while __start_block < __end_block + 1:
+  #  block_trans.update(rpc.get_transactions(__start_block)[0])
+  #  __start_block += 1
 
   # filter all transactions that contain a field OP_RETURN 
-  find_block_trans = core.find_op_return(block_trans)
+  #find_block_trans = core.find_op_return(block_trans)
+  #return find_block_trans
+
+#  s = {}
+#  for i in range(3):
+#    s.update({ i : (str(start) + " - "+ str(end))})
+#  print(s)
+#  return s
+
+
+
+
+#def write_sql(__databaseFile, find_block_trans):
   # save all transaction with a OP_RETURN field
-  core.save_result_in_database(__databaseFile, find_block_trans)
+  #core.save_result_in_database(__databaseFile, find_block_trans)
+#  for i in range(len(find_block_trans)):
+#    print(i)
 
   # create diagram by using created database
-  #diagram.create_diagrams()
+  diagram.create_diagrams()
+
--- process.py
+++ process.py
@@ -1,10 +1,37 @@
-from threading import Thread 
+import threading 
 import main
+import config
+
+
+
+threads = []
+result =[]
+
+def fun_thrd(s,e):
+    result.append(main.thread_fun(s,e))
+
 
 e = 315000
 r = 2000
+#while (e < 400000) or (len(result) != 0):
+#    if(e<400000):
+#        # start threads 
+#        for k in range(10):
+#            s = e + 1 
+#            e = e + r 
+#            thr = threading.Thread(target=fun_thrd, args=(s,e))
+#            threads.append(thr)
+        # Start them all
+#        for thread in threads:
+#            thread.start()
+#        for t in threads:
+#            t.join()
 
-for i in range(10):
-    s = e 
-    e = e + r 
-    Thread(target=main.thread_fun, args=(s, e )).start()
+
+    # store in sqlite
+#    for i in result:
+#        main.write_sql(config.CONFIG['database_file_name'], i)
+
+#    threads = []
+#    result = []
+
