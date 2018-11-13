14e9d009a01f44bbce7f541e78427aebe83c4d56
Meric <doa_s@live.de>
2018-11-13 20:22:13 +0100

    no message

--- Diagram/hex_converting.py
+++ Diagram/hex_converting.py
@@ -10,19 +10,19 @@ f1 = open('a.txt','w')
 def check_hex(arrayList):
     # parameters for counting different contents
   
-  
-    counter = np.zeros(9)   
+    # c[0] count content with only 'OP_RETURN '
+    # c[1] count error content
+    # c[2] count content not hex
+    # c[3] count content odd length
+    # c[4] count content website
+    # c[5] count content  metadata
+    # c[6] count content  digit
+    # c[7] count content  text
+    # c[8] count content  undefinable
+
+    c = np.zeros(9)   
 
 
-    count_http = 0
-    count_txt = 0
-    count_ud = 0
-    count_dig = 0
-    count_doc = 0
-    count_op = 0
-    count_odd = 0
-    count_error = 0
-    count_not_hex = 0
 
     # in given array[array] check what content an OP_RETRUN has
     for i in arrayList:
@@ -31,17 +31,22 @@ def check_hex(arrayList):
         if not(hf.is_hex(j)):
             # check content is 'OP_RETURN'
             if hf.is_OP(j):
-                count_op = count_op + 1
+                c[0] = c[0] + 1
             # check content is '[error]'
             elif '[error]' in str(j):
-                count_error = count_error + 1
+                c[1] = c[1] + 1
             else:
-                count_not_hex = count_not_hex + 1  
+                c[2] = c[2] + 1  
+
+        
         # hex is odd length add '0' at beginning
         elif len(j) %2 != 0:
             j = '0' + j
-            count_odd = count_odd +1
+            c[3] = c[3] +1
         # hex is even length check content of OP_RETURN hex
+        
+        
+        
         else:
             # convert hexstring to binary data
             binary = by.unhexlify(j) 
@@ -50,49 +55,51 @@ def check_hex(arrayList):
                 bin_dec =binary.decode(__ASCII__)     
                 # check content is website/email address
                 if hf.check_website(bin_dec):
-                    count_http = count_http + 1
+                    c[4] = c[4] + 1
                 # check if content is document/proof of existent etc. 
                 elif hf.is_metadata(bin_dec):
-                    count_doc = count_doc + 1
+                    c[5] = c[5] + 1
                 # check content is digit
                 elif  hf.hex_int(bin_dec):
-                    count_dig += 1
+                    c[6] = c[6] +1
                 # check content is text message
                 elif  (hf.is_ascii(bin_dec)) and (' ' in bin_dec):
-                    count_txt = count_txt + 1
+                    c[7] = c[7] + 1
                 elif (hf.is_ascii(bin_dec) and hf.no_digit(bin_dec)):
-                    count_txt = count_txt + 1
+                    c[7] = c[7] + 1
                 elif hf.is_text(bin_dec):
                     # check if content is not definable but is ascii
-                    count_txt= count_txt +1
+                    c[7]= c[7] +1
+            
+            
             except:
                 a = str(binary)[2:-1]
                 try:
                     # check binary data contains url 
                     if hf.check_website(str(binary)):
-                        count_http = count_http + 1 
+                        c[4] = c[4] + 1 
                     # check binary data contains document 
                     elif hf.is_metadata(str(binary)):
-                        count_doc = count_doc + 1
+                        c[5] = c[5] + 1
                 # hex not decodable 
                 except:
                     if hf.is_metadata_hex(j):
-                        count_doc = count_doc +1
+                        c[5] = c[5] +1
                     # else binary data not definable
                     elif hf.hex_int(a):
-                        count_dig = count_dig + 1
+                        c[6] = c[6] + 1
                     elif hf.is_hex_op(a) or hf.undef_hexstring(a):
-                        count_ud = count_ud +1
+                        c[8] = c[8] +1
                         i.append(len(j)/2)
                         hf.save_op_sql(i)
                     elif hf.is_ascii(a) and hf.no_digit(a):
-                        count_txt = count_txt + 1
+                        c[7] = c[7] + 1
                     elif hf.is_ascii(a) and not(hf.no_digit(a)):
-                        count_txt = count_txt +1
+                        c[7] = c[7] +1
                     elif hf.is_text(a):
-                        count_txt = count_txt + 1
+                        c[7] = c[7] + 1
                     else:
-                        count_ud = count_ud +1
+                        c[8] = c[8] +1
                         i.append(len(j)/2)
                         hf.save_op_sql(i)
 
@@ -100,11 +107,7 @@ def check_hex(arrayList):
     x = ['Empty',  'Error',     'Not Hex',    'Odd Lenght', 'Website',   
         'Number',  'Text',    'DOCPROOF', 'Not decodable']
 
-    #  (_,y) part of a tuple --> name of found contents
-    y = [count_op, count_error, count_not_hex, count_odd, count_http, 
-        count_dig, count_txt, count_doc,  count_ud]
-
     # concatinate found solutions in a list and return it
-    ascii = list(zip(x,y))
+    ascii = list(zip(x,c))
     return ascii
 
