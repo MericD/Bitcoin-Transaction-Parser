3bad02351f0e0a18a8df3438f9d7ed6db8ebf88c
Meric <doa_s@live.de>
2018-11-23 16:22:54 +0100

    no message

--- Core/core.py
+++ Core/core.py
@@ -148,7 +148,7 @@ def get_sender_address_of_op_tx(value):
     # search in "vout" (decoded raw transaction information) for value-field 
     for k , v in value.items():
         for i in range(len(v["vin"])):
-            if ["scriptSig"] in value:
+            if "scriptSig" in value:
                 potential_sender_add = v["vin"][i]["scriptSig"]["asm"]
                 potential_sender_add = re.sub(r'.* ', '', potential_sender_add)
                 a = addr.PubkeyToAddress(potential_sender_add)
